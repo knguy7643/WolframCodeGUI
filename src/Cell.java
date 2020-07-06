8f8c964c27466018531d0036df35f010712577c0
knguy7643 <knguyen7643@gmail.com>
2020-07-06 14:26:10 -0500

    Cell method bodies and comments.

--- src/Cell.java
+++ src/Cell.java
@@ -1,13 +1,39 @@
 
 public class Cell {
 	
+	/**
+	 * This field stores the CellState of the Cell.
+	 */
 	private CellState cellstate;
 	
+	/**
+	 * This default constructor creates a Cell with the OFF CellState.
+	 */
 	public Cell() {
 		this.cellstate = CellState.OFF;
 	}
 	
+	/**
+	 * This constructor creates a cell with the desired CellState.
+	 * @param cellState The CellState desired for the cell.
+	 */
 	public Cell(CellState cellState) {
-		
+		this.cellstate = cellState;
+	}
+	
+	/**
+	 * This method returns the CellState stored by the cell.
+	 * @return The CellState of the cell.
+	 */
+	public CellState getState() {
+		return this.cellstate;
+	}
+	
+	/**
+	 * This method returns the Cell's CellState in its string representation.
+	 * @return The string representation of the Cell's CellState.
+	 */
+	public String toString() {
+		return this.cellstate.toString();
 	}
 }
