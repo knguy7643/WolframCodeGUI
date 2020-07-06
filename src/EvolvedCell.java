5bcc268d30853d94a2bb17f69a7332e8a8797387
knguy7643 <knguyen7643@gmail.com>
2020-07-06 14:49:06 -0500

    EvolvedCell methods and comments.

--- src/EvolvedCell.java
+++ src/EvolvedCell.java
@@ -1,4 +1,27 @@
 
 public class EvolvedCell extends Cell {
 
+	/**
+	 * This field stores the subrule number used to determine the CellState of this EvolvedCell.
+	 */
+	private int subRuleNumber;
+	
+	/**
+	 * This method returns the subrule number of the EvolvedCell.
+	 * @return The subrule number stored by the EvolvedCell.
+	 */
+	public int getSubruleNum() {
+		return this.subRuleNumber;
+	}
+	
+	/**
+	 * This constructor creates an EvolvedCell using a specified CellState and subrule number/
+	 * @param cellState The desired CellState for the EvolvedCell.
+	 * @param subRuleNum The subrule number used to determine the CellState for the Evolved Cell.
+	 */
+	public EvolvedCell(CellState cellState, int subRuleNum) {
+		super(cellState);
+		this.subRuleNumber = subRuleNum;
+	}
+	
 }
